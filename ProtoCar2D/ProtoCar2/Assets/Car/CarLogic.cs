1ac823a03784c59d19e4d5113ca5f6800a8f7628
Pol <pol.valverde@enti.cat>
2023-05-30 00:11:56 +0200

    dfdafsdfdfdf

--- ProtoCar2D/ProtoCar2/Assets/Car/CarLogic.cs
+++ ProtoCar2D/ProtoCar2/Assets/Car/CarLogic.cs
@@ -8,11 +8,18 @@ public class CarLogic : MonoBehaviour
 
     [SerializeField] float accelerationPower = 5f;
     [SerializeField] float steeringPower = 5f;
-    float steeringAmount, speed, direction;
-
+    [SerializeField] Vector2 startPos = new Vector2(1,3.6f);
+    [SerializeField] GameObject anim;
+    bool justCollided;
+    float steeringAmount, speed, direction, tempSteer;
+    Quaternion startRot;
+    //Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        
+        startRot = gameObject.transform.rotation;
+        tempSteer = steeringPower;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -22,16 +29,48 @@ public class CarLogic : MonoBehaviour
         steeringAmount = -Input.GetAxis("Horizontal");
         if(steeringAmount >= 1f)
         {
-            speed = accelerationPower -2;
+           
+            speed = accelerationPower -2.7f;
+            steeringPower += 0.3f * Time.deltaTime;
         }
         else
         {
+              
+            steeringPower = tempSteer;
             speed = accelerationPower;
+
         }
         direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
         rb.rotation += steeringAmount * steeringPower * rb.velocity.magnitude * direction;
 
         rb.AddRelativeForce(Vector2.up * speed);
         rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "spline")
+        {
+            
+            StartCoroutine(Die());
+            
+        }
+    }
+    IEnumerator Die()
+    {
+        if(justCollided == false)
+        {
+            
+            justCollided = true;
+           
+            yield return new WaitForSeconds(1.0f);
+            rb.velocity = Vector2.zero;
+            steeringAmount = 0;
+            
+            gameObject.transform.rotation = startRot;
+            transform.position = startPos;
+            justCollided = false;
+        }
+        
     }
 }
